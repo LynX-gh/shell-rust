a89baa683b54b2894b1330ef96bc198124e26887
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-11 01:43:36 +0530

    The exit builtin

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,37 @@
-use core::fmt;
 #[allow(unused_imports)]
+use core::fmt;
 use std::io::{self, Write};
+use std::collections::VecDeque;
+
+trait StringExt {
+    fn trim_newline(self: &mut Self);
+}
+
+impl StringExt for String {
+    // Function to trim the newline at the end of the command entered by the user
+    fn trim_newline(&mut self) {
+        while self.ends_with('\n') {
+            self.pop();
+            while self.ends_with('\r') {
+                self.pop();
+            }
+        }
+    }
+}
+
+fn handle_user_input(inputs: &mut VecDeque<&str>) -> String {
+    if let Some(command) = inputs.pop_front() {
+        match command {
+            "exit" => {
+                let exit_code = if let Some(exit_code) = inputs.pop_front() { exit_code.parse::<i32>().unwrap_or(0) } else { 0 };
+                std::process::exit(exit_code);
+            },
+            _ => format!("{command}: command not found"),
+        }
+    } else {
+        "".to_string()
+    }
+}
 
 fn main() {
     loop {
@@ -8,34 +39,16 @@ fn main() {
         io::stdout().flush().unwrap();
 
         // Wait for user input
-        let stdin = io::stdin();
         let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
+        io::stdin().read_line(&mut input).unwrap();
+        input.trim_newline();
+        let mut inputs = input.split_whitespace().collect::<VecDeque<_>>();
 
         // Handle User Input
-        trim_newline(&mut input);
-        let output = handle_user_input(& input);
+        let output = handle_user_input(&mut inputs);
         io::stdout().write(output.as_bytes()).unwrap();
 
         // Print a newline
         io::stdout().write("\n".as_bytes()).unwrap();
-    }
-}
-
-fn handle_user_input(input: &str) -> String {
-    format!("{input}: command not found")
-    // match input {
-    //     "exit" => std::process::exit(0),
-    //     _ => format!("{input}: command not found"),
-    // }
-}
-
-// Function to trim the newline at the end of the command entered by the user
-fn trim_newline(s: &mut String) {
-    while s.ends_with('\n') {
-        s.pop();
-        while s.ends_with('\r') {
-            s.pop();
-        }
     }
 }
