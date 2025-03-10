b3f8f7c171101e2e78fe9e0171522e332f51deb1
lynx <76166621+LynX-gh@users.noreply.github.com>
2025-03-11 01:18:32 +0530

    REPL

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,38 @@ use core::fmt;
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let stdin = io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).unwrap();
+        // Wait for user input
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
 
-    // Handle Invalid input
-    trim_newline(&mut input);
-    let output = format!("{input}: command not found");
-    io::stdout().write(output.as_bytes()).unwrap();
+        // Handle User Input
+        trim_newline(&mut input);
+        let output = handle_user_input(& input);
+        io::stdout().write(output.as_bytes()).unwrap();
+
+        // Print a newline
+        io::stdout().write("\n".as_bytes()).unwrap();
+    }
+}
+
+fn handle_user_input(input: &str) -> String {
+    format!("{input}: command not found")
+    // match input {
+    //     "exit" => std::process::exit(0),
+    //     _ => format!("{input}: command not found"),
+    // }
 }
 
 // Function to trim the newline at the end of the command entered by the user
 fn trim_newline(s: &mut String) {
-    if s.ends_with('\n') {
+    while s.ends_with('\n') {
         s.pop();
-        if s.ends_with('\r') {
+        while s.ends_with('\r') {
             s.pop();
         }
     }
